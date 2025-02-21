1e58e93f04f033d09257b44ccab51bab1eb9707d
ductu <tund@mor.com.vn>
2025-02-22 00:00:39 +0700

    update: tu and phuong

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify
-from telegram import Bot
-import urllib.parse
-from flask_cors import CORS
 import requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from telegram import Bot
 
 app = Flask(__name__)
 CORS(app)
 
-
 TOKEN = ''
 CHAT_ID = ''
+
+
 def sendBot(name, content, confirm):
     bot = Bot(TOKEN)
-    text= "Name: " + name + "\n" + "Sent you a wish: " + content + "\n" + confirm
+    text = "Name: " + name + "\n" + "Sent you a wish: " + content + "\n" + confirm
     try:
         bot.sendMessage(
             CHAT_ID,
@@ -21,11 +21,12 @@ def sendBot(name, content, confirm):
     except:
         print("Looks like CHAT_ID or TOKEN of telegram-bot was wrong!")
 
+
 @app.route('/wishes', methods=['POST'])
 def wishes():
     turnstile_token = request.form.get("cf-turnstile-response")
     secret_key = ""
-    #print(turnstile_token)
+    # print(turnstile_token)
 
     try:
         response = requests.post(
@@ -35,7 +36,7 @@ def wishes():
         response_data = response.json()
         print(response_data.get("success"))
         if response_data.get("success"):
-            data =  request.form.to_dict()
+            data = request.form.to_dict()
             name = request.form.get('name')
             content = request.form.get('content')
             if "confirm" not in data:
@@ -51,6 +52,7 @@ def wishes():
             return jsonify({"message": "Turnstile verification failed"}), 400
     except Exception as e:
         return jsonify({"message": "Error verifying Turnstile token"}), 500
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="8080")
